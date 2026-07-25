0a613de02dc51c254e40d26f09307d7edce4cae1
premiumdates <mrshahbaz4890@gmail.com>
2026-07-26 01:26:28 +0500

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,1 +1,30 @@
+import { auth } from "./firebase.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+const signupBtn = document.getElementById("signupBtn");
+
+signupBtn.addEventListener("click", async () => {
+
+  if (password.value !== confirmPassword.value) {
+    alert("Passwords do not match!");
+    return;
+  }
+
+  try {
+    await createUserWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value
+    );
+
+    alert("Account Created Successfully!");
+    window.location.href = "login.html";
+
+  } catch (error) {
+    alert(error.message);
+  }
+
+});
