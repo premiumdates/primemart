a9457a7c3cc566c545f46ae367e8788dbb824e33
premiumdates <mrshahbaz4890@gmail.com>
2026-07-26 01:33:17 +0500

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,22 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", async () => {
+  try {
+    await signInWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value
+    );
+
+    alert("Login Successful!");
+    window.location.href = "index.html";
+
+  } catch (error) {
+    alert(error.message);
+  }
+});
