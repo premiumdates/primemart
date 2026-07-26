6b8482caaceb22c67f6f5479748013f793f8b89c
primemartstore <mrshahbaz4890@gmail.com>
2026-07-26 16:18:16 +0500

    Update seller.js

--- seller.js
+++ seller.js
@@ -65,7 +65,7 @@ async function loadProducts() {
 
 loadProducts();
 addBtn.onclick = async function () {
-
+alert("Button Clicked");
   const product = {
     name: nameInput.value,
     price: priceInput.value,
