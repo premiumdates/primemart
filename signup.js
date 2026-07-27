fbcbb35b3c320ecf2abd427bc3dc7c76ea0fd538
primemartstore <mrshahbaz4890@gmail.com>
2026-07-27 09:59:21 +0500

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,4 +1,4 @@
-import { auth } from "./firebase.js";
+import { auth } from "./firebase-config.js";
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const email = document.getElementById("email");
