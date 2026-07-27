e9d4c5b923d3214c7ece6831d6a1c8e5e1bbc03f
primemartstore <mrshahbaz4890@gmail.com>
2026-07-27 20:10:14 +0500

    Update seller-dashboard.js

--- seller-dashboard.js
+++ seller-dashboard.js
@@ -17,3 +17,34 @@ logoutBtn.addEventListener("click", () => {
     window.location.href = "login.html";
 
 });
+// Product Form
+const productForm = document.getElementById("productForm");
+
+productForm.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    const inputs = productForm.querySelectorAll("input, textarea");
+
+    const product = {
+
+        name: inputs[0].value,
+        category: inputs[1].value,
+        price: inputs[2].value,
+        discount: inputs[3].value,
+        stock: inputs[4].value,
+        description: inputs[5].value
+
+    };
+
+    let products = JSON.parse(localStorage.getItem("products")) || [];
+
+    products.push(product);
+
+    localStorage.setItem("products", JSON.stringify(products));
+
+    alert("Product Saved Successfully!");
+
+    productForm.reset();
+
+});
