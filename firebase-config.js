318ac1c8763d3f810ac2fdee1af7e1c38b35a5be
primemartstore <mrshahbaz4890@gmail.com>
2026-07-27 23:37:10 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,48 @@
-// Firebase Config
+// ==============================
+// PrimeMart Firebase Config
+// ==============================
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
 import {
-  getAuth
+    getAuth
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-// اپنا Firebase Config یہاں بعد میں ڈالیں
+import {
+    getDatabase
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+import {
+    getStorage
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAXMl5u5PekmZsSB7tYFyibitVJsnJEF10",
-  authDomain: "primemart-6a101.firebaseapp.com",
-  databaseURL: "https://primemart-6a101-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "primemart-6a101",
-  storageBucket: "primemart-6a101.firebasestorage.app",
-  messagingSenderId: "675471841308",
-  appId: "1:675471841308:web:0a3a861890a820072ce7d9",
-  measurementId: "G-HPGWP68Z9L"
+
+    apiKey: "AIzaSyAXMl5u5PekmZsSB7tYFyibitVJsnJEF10",
+
+    authDomain: "primemart-6a101.firebaseapp.com",
+
+    databaseURL: "https://primemart-6a101-default-rtdb.asia-southeast1.firebasedatabase.app",
+
+    projectId: "primemart-6a101",
+
+    storageBucket: "primemart-6a101.firebasestorage.app",
+
+    messagingSenderId: "675471841308",
+
+    appId: "1:675471841308:web:0a3a861890a820072ce7d9",
+
+    measurementId: "G-HPGWP68Z9L"
+
 };
 
 const app = initializeApp(firebaseConfig);
+
+// Authentication
 export const auth = getAuth(app);
+
+// Realtime Database
+export const db = getDatabase(app);
+
+// Firebase Storage
+export const storage = getStorage(app);
