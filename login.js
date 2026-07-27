a23889cf89a53f47150b04b5fe16cc7c1a6d56cd
primemartstore <mrshahbaz4890@gmail.com>
2026-07-27 22:49:25 +0500

    Update login.js

--- login.js
+++ login.js
@@ -1,45 +1,64 @@
 import { auth } from "./firebase-config.js";
+
 import {
-  GoogleAuthProvider,
-  signInWithPopup
+    GoogleAuthProvider,
+    signInWithPopup,
+    signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const loginBtn = document.getElementById("loginBtn");
+const googleLoginBtn = document.getElementById("googleLoginBtn");
 
 loginBtn.addEventListener("click", async () => {
-  try {
-    await signInWithEmailAndPassword(
-      auth,
-      email.value,
-      password.value
-    );
 
-    alert("Login Successful!");
-localStorage.setItem("currentUser", email.value);
-// User کو Login رکھو
-localStorage.setItem("currentUser", auth.currentUser.uid);
-localStorage.setItem("userEmail", auth.currentUser.email);
+    try {
 
-window.location.href = "choose-account.html";
+        const userCredential = await signInWithEmailAndPassword(
 
-  } catch (error) {
-    alert(error.message);
-  }
+            auth,
+            email.value,
+            password.value
+
+        );
+
+        const user = userCredential.user;
+
+        localStorage.setItem("currentUser", user.uid);
+        localStorage.setItem("userEmail", user.email);
+
+        alert("Login Successful!");
+
+        window.location.href = "choose-account.html";
+
+    } catch (error) {
+
+        alert(error.message);
+
+    }
+
 });
 
 const provider = new GoogleAuthProvider();
 
-document.getElementById("googleLoginBtn").addEventListener("click", async () => {
-  try {
-    const result = await signInWithPopup(auth, provider);
-    localStorage.setItem("currentUser", result.user.uid);
-localStorage.setItem("userEmail", result.user.email);
-    alert("Welcome " + result.user.displayName);
-    window.location.href = "choose-account.html";
-  } catch (error) {
-    alert(error.message);
-  }
+googleLoginBtn.addEventListener("click", async () => {
+
+    try {
+
+        const result = await signInWithPopup(auth, provider);
+
+        localStorage.setItem("currentUser", result.user.uid);
+        localStorage.setItem("userEmail", result.user.email);
+
+        alert("Welcome " + result.user.displayName);
+
+        window.location.href = "choose-account.html";
+
+    } catch (error) {
+
+        alert(error.message);
+
+    }
+
 });
