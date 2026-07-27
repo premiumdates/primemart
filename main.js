295dd7c57137805db44e8ee41433651704a693ac
primemartstore <mrshahbaz4890@gmail.com>
2026-07-27 11:45:58 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
+import { auth } from "./firebase-config.js";
+import {
+    onAuthStateChanged,
+    signOut
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+const guestButtons = document.getElementById("guestButtons");
+const userMenu = document.getElementById("userMenu");
+const logoutBtn = document.getElementById("logoutBtn");
+
+onAuthStateChanged(auth, (user) => {
+
+    if (user) {
+
+        if (guestButtons)
+            guestButtons.style.display = "none";
+
+        if (userMenu)
+            userMenu.style.display = "flex";
+
+    } else {
+
+        if (guestButtons)
+            guestButtons.style.display = "flex";
+
+        if (userMenu)
+            userMenu.style.display = "none";
+
+    }
+
+});
+
+if (logoutBtn) {
+
+    logoutBtn.onclick = async () => {
+
+        await signOut(auth);
+
+        window.location.href = "index.html";
+
+    };
+
+}
