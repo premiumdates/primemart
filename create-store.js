fcf8bbdfbe05a1dbe35046a575e871cfa8d1e226
primemartstore <mrshahbaz4890@gmail.com>
2026-07-27 07:26:38 +0500

    Update create-store.js

--- create-store.js
+++ create-store.js
@@ -1,1 +1,19 @@
+import { auth } from "./firebase.js";
 
+const form = document.getElementById("createStoreForm");
+const storeName = document.getElementById("storeName");
+const paymentMethod = document.getElementById("paymentMethod");
+const paymentNumber = document.getElementById("paymentNumber");
+const storeNameStatus = document.getElementById("storeNameStatus");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    if (!auth.currentUser) {
+        alert("Please login first.");
+        return;
+    }
+
+    alert("Store Created Successfully!");
+    window.location.href = "seller-dashboard.html";
+});
