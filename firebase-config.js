974a68768c9788b651e6a4c8a0c986e3e0b447b2
primemartstore <mrshahbaz4890@gmail.com>
2026-07-27 09:53:57 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,12 +6,14 @@ import {
 
 // اپنا Firebase Config یہاں بعد میں ڈالیں
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAXMl5u5PekmZsSB7tYFyibitVJsnJEF10",
+  authDomain: "primemart-6a101.firebaseapp.com",
+  databaseURL: "https://primemart-6a101-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "primemart-6a101",
+  storageBucket: "primemart-6a101.firebasestorage.app",
+  messagingSenderId: "675471841308",
+  appId: "1:675471841308:web:0a3a861890a820072ce7d9",
+  measurementId: "G-HPGWP68Z9L"
 };
 
 const app = initializeApp(firebaseConfig);
